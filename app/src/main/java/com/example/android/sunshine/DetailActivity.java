0f2a323c7d869485d734285e286c7e4cd0a5bb6c
onofresalvador <onofresalvador01@gmail.com>
2018-02-16 19:36:43 -0500

    Showing your pick in detail activity intent putextra

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,30 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
-
+    private TextView mdisplayResult;
+    private String forcast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+         mdisplayResult = (TextView)findViewById(R.id.display_weather);
         // TODO (2) Display the weather forecast that was passed from MainActivity
+
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null){
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)){
+                   forcast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                   mdisplayResult.setText(forcast);
+            }
+        }
     }
 }
