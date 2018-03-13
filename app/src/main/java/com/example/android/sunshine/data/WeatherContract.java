067f78baa01e32604beceac5b9f9407493fc438e
onofresalvador <onofresalvador01@gmail.com>
2018-03-13 19:52:50 -0400

    Created a sqlite database for the app

--- app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.sunshine.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Defines table and column names for the weather database. This class is not necessary, but keeps
  * the code organized.
@@ -22,23 +24,43 @@ package com.example.android.sunshine.data;
 public class WeatherContract {
 
 //  TODO (1) Within WeatherContract, create a public static final class called WeatherEntry that implements BaseColumns
+public static final class WeatherEntry implements BaseColumns {
 
 //      Do steps 2 through 10 within the WeatherEntry class
 
 //      TODO (2) Create a public static final String call TABLE_NAME with the value "weather"
 
-//      TODO (3) Create a public static final String call COLUMN_DATE with the value "date"
+    /* Used internally as the name of our weather table. */
+    public static final String TABLE_NAME = "weather";
 
-//      TODO (4) Create a public static final String call COLUMN_WEATHER_ID with the value "weather_id"
+    //      TODO (3) Create a public static final String call COLUMN_DATE with the value "date"
 
-//      TODO (5) Create a public static final String call COLUMN_MIN_TEMP with the value "min"
-//      TODO (6) Create a public static final String call COLUMN_MAX_TEMP with the value "max"
+    public static final String COLUMN_DATE = "date";
 
-//      TODO (7) Create a public static final String call COLUMN_HUMIDITY with the value "humidity"
+    //      TODO (4) Create a public static final String call COLUMN_WEATHER_ID with the value "weather_id"
+        /* Weather ID as returned by API, used to identify the icon to be used */
+    public static final String COLUMN_WEATHER_ID = "weather_id";
 
-//      TODO (8) Create a public static final String call COLUMN_PRESSURE with the value "pressure"
+    //      TODO (5) Create a public static final String call COLUMN_MIN_TEMP with the value "min"
+        /* Min and max temperatures in °C for the day (stored as floats in the database) */
+    public static final String COLUMN_MIN_TEMP = "min";
+    //      TODO (6) Create a public static final String call COLUMN_MAX_TEMP with the value "max"
+    public static final String COLUMN_MAX_TEMP = "max";
 
-//      TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
+    //      TODO (7) Create a public static final String call COLUMN_HUMIDITY with the value "humidity"
+        /* Humidity is stored as a float representing percentage */
+    public static final String COLUMN_HUMIDITY = "humidity";
 
-//      TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
+    //      TODO (8) Create a public static final String call COLUMN_PRESSURE with the value "pressure"
+        /* Pressure is stored as a float representing percentage */
+    public static final String COLUMN_PRESSURE = "pressure";
+
+    //      TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
+        /* Wind speed is stored as a float representing wind speed in mph */
+    public static final String COLUMN_WIND_SPEED = "wind";
+
+    //      TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
+
+    public static final String COLUMN_DEGREES = "degrees";
+}
 }
